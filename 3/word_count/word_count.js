d802fa53c09e29652ab48f112708ae5b93f0c01c
jsculsp <328232234@qq.com>
2017-10-10 11:35:01 +0800

    save

--- 3/word_count/word_count.js
+++ 3/word_count/word_count.js
@@ -1,47 +1,50 @@
-var fs = require('fs');
-var completedTasks = 0;
-var tasks = [];
-var wordCounts = {};
-var filesDir = './text';
+const fs = require('fs')
+const filesDir = './text'
 
-function checkIfComplete() {
-  completedTasks++;
-  if (completedTasks == tasks.length) {
-    for(var index in wordCounts) { 
-      console.log(index +': ' + wordCounts[index]);
+let completedTasks = 0
+let tasks = []
+let wordCounts = {}
+
+const checkIfComplete = function () {
+    completedTasks++
+    if (completedTasks == tasks.length) {
+        for (let index in wordCounts) {
+            console.log(index + ': ' + wordCounts[index])
+        }
     }
-  }
 }
 
-function countWordsInText(text) {
-  var words = text
-    .toString()
-    .toLowerCase()
-    .split(/\W+/)
-    .sort();
-  for(var index in words) { 
-    var word = words[index];
-    if (word) {
-      wordCounts[word] = (wordCounts[word]) ? wordCounts[word] + 1 : 1;
+const countWordsInText = function (text) {
+    let words = text
+        .toString()
+        .toLowerCase()
+        .split(/\W+/)
+        .sort()
+    for (let index in words) {
+        let word = words[index]
+        if (word) {
+            wordCounts[word] = (wordCounts[word]) ? wordCounts[word] + 1 : 1
+        }
     }
-  }
 }
 
-fs.readdir(filesDir, function(err, files) { 
-  if (err) throw err;
-  for(var index in files) {
-    var task = (function(file) { 
-      return function() {
-        fs.readFile(file, function(err, text) {
-          if (err) throw err;
-          countWordsInText(text);
-          checkIfComplete();
-        });
-      }
-    })(filesDir + '/' + files[index]);
-    tasks.push(task); 
-  }
-  for(var task in tasks) { 
-    tasks[task]();
-  }
-});
+const genTask = function (file) {
+    return function () {
+        fs.readFile(file, function (err, text) {
+            if (err) throw err
+            countWordsInText(text)
+            checkIfComplete()
+        })
+    }
+}
+
+fs.readdir(filesDir, (err, files) => {
+    if (err) throw err
+    for (let f of files) {
+        let task = genTask(filesDir + '/' + f)
+        tasks.push(task)
+    }
+    for (let t of tasks) {
+        t()
+    }
+})
