089e0b4ad8afc9d1f4ae6286fe47c913dadf5c06
jsculsp <328232234@qq.com>
2017-10-11 11:31:36 +0800

    save

--- 5/redis_pubsub_chat.js
+++ 5/redis_pubsub_chat.js
@@ -1,30 +1,25 @@
-var net = require('net');
-var redis = require('redis');
+const net = require('net')
+const redis = require('redis')
 
-var server = net.createServer(function(socket) {
-  var subscriber;
-  var publisher;
+const server = net.createServer(function (socket) {
+    let subscriber = redis.createClient()
+    subscriber.subscribe('main_chat_room')
 
-  socket.on('connect', function() {
-    subscriber = redis.createClient();
-    subscriber.subscribe('main_chat_room');
+    subscriber.on('message', function (channel, message) {
+        socket.write('Channel ' + channel + ': ' + message)
+    })
 
-    subscriber.on('message', function(channel, message) {
-      socket.write('Channel ' + channel + ': ' + message);
-    });
+    let publisher = redis.createClient()
 
-    publisher = redis.createClient();
-  });
+    socket.on('data', function (data) {
+        publisher.publish('main_chat_room', data)
+    })
 
-  socket.on('data', function(data) {
-    publisher.publish('main_chat_room', data);
-  });
+    socket.on('end', function () {
+        subscriber.unsubscribe('main_chat_room')
+        subscriber.end()
+        publisher.end()
+    })
+})
 
-  socket.on('end', function() {
-    subscriber.unsubscribe('main_chat_room');
-    subscriber.end();
-    publisher.end();
-  });
-});
-
-server.listen(3000);
+server.listen(3000)
