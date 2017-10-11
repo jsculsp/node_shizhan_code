ae358d11749ac34ab6d69c380625e5f9215d42da
jsculsp <328232234@qq.com>
2017-10-11 10:42:01 +0800

    save

--- 5/redis_basics.js
+++ 5/redis_basics.js
@@ -1,38 +1,42 @@
-var redis = require('redis');
-var client = redis.createClient(6379, '192.168.3.108');
+const redis = require('redis')
+const RDS_PORT = 6379
+const RDS_HOST = '192.168.3.108'
+const RDS_PWD = 'DhG7n&5q'
+const RDS_OPTS = {auth_pass: RDS_PWD}
+const client = redis.createClient(RDS_PORT, RDS_HOST, RDS_OPTS)
 
 client.on('error', function (err) {
-    console.log('Error ' + err);
-});
+    console.log('Error ' + err)
+})
 
-client.set('color', 'red', redis.print);
+client.set('color', 'red', redis.print)
 client.get('color', function (err, value) {
-    if (err) throw err;
-    console.log('Got: ' + value);
-});
+    if (err) throw err
+    console.log('Got: ' + value)
+})
 
 client.hmset('camping', {
     'shelter': '2-person tent',
     'cooking': 'campstove'
-}, redis.print);
+}, redis.print)
 
 client.hget('camping', 'cooking', function (err, value) {
-    if (err) throw err;
-    console.log('Will be cooking with: ' + value);
-});
+    if (err) throw err
+    console.log('Will be cooking with: ' + value)
+})
 
 client.hkeys('camping', function (err, keys) {
-    if (err) throw err;
+    if (err) throw err
     keys.forEach(function (key, i) {
-        console.log(' ' + key);
-    });
-});
+        console.log(' ' + key)
+    })
+})
 
-client.lpush('tasks', 'Paint the bikeshed red.', redis.print);
-client.lpush('tasks', 'Paint the bikeshed green.', redis.print);
+client.lpush('tasks', 'Paint the bikeshed red.', redis.print)
+client.lpush('tasks', 'Paint the bikeshed green.', redis.print)
 client.lrange('tasks', 0, -1, function (err, items) {
-    if (err) throw err;
+    if (err) throw err
     items.forEach(function (item, i) {
-        console.log(' ' + item);
-    });
-});
+        console.log(' ' + item)
+    })
+})
