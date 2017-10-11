ac90470ad75733ce47ecd3a3c2692bfd79686f84
jsculsp <328232234@qq.com>
2017-10-11 17:52:20 +0800

    save

--- 5/mongoose1.js
+++ 5/mongoose1.js
@@ -9,6 +9,8 @@ let Tasks = new Schema({
 mongoose.model('Task', Tasks)
 
 let Task = mongoose.model('Task')
+
+// 运行其它函数前把这个函数注释掉
 let task = new Task()
 task.project = 'Bikeshed'
 task.description = 'Paint the bikeshed red.'
@@ -17,9 +19,24 @@ task.save(function (err) {
     console.log('Task saved.')
 })
 
+// 运行其它函数前把这个函数注释掉
 Task.find({'project': 'Bikeshed'}, function (err, tasks) {
     for (let i = 0; i < tasks.length; i++) {
         console.log('ID:' + tasks[i]._id)
         console.log(tasks[i].description)
     }
 })
+
+// 运行其它函数前把这个函数注释掉
+Task.update(
+    {_id: '59dde6c0d2b669566cfe3c9c'},
+    {description: 'Paint the bike green.'},
+    {multi: false},
+    (err, rows_updated) => {
+        if (err) {
+            throw err
+        }
+
+        console.log(`${rows_updated} updated!`)
+    }
+)
