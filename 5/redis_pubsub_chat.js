dbe890869200c5b8922272cfac522564780c835f
jsculsp <328232234@qq.com>
2017-10-11 13:07:57 +0800

    save

--- 5/redis_pubsub_chat.js
+++ 5/redis_pubsub_chat.js
@@ -1,15 +1,16 @@
 const net = require('net')
 const redis = require('redis')
 
-const server = net.createServer(function (socket) {
-    let subscriber = redis.createClient()
+const server = net.createServer()
+
+server.on('connection', (socket) => {
+    let subscriber = redis.createClient()   // 订阅者
+    let publisher = redis.createClient()    // 发布者
     subscriber.subscribe('main_chat_room')
 
     subscriber.on('message', function (channel, message) {
         socket.write('Channel ' + channel + ': ' + message)
     })
-
-    let publisher = redis.createClient()
 
     socket.on('data', function (data) {
         publisher.publish('main_chat_room', data)
