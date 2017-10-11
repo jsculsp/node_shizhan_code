2a97182c44fd09c539c9ef5a9e3556806a8eed54
jsculsp <328232234@qq.com>
2017-10-11 17:40:52 +0800

    save

--- 5/mongoose1.js
+++ 5/mongoose1.js
@@ -1,26 +1,26 @@
-var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/tasks');
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/tasks')
 
-var Schema = mongoose.Schema;
-var Tasks = new Schema({
-  project: String,
-  description: String
-});
-mongoose.model('Task', Tasks);
+let Schema = mongoose.Schema
+let Tasks = new Schema({
+    project: String,
+    description: String
+})
+mongoose.model('Task', Tasks)
 
-var Task = mongoose.model('Task');
-var task = new Task();
-task.project = 'Bikeshed';
-task.description = 'Paint the bikeshed red.';
-task.save(function(err) {
-  if (err) throw err;
-  console.log('Task saved.');
-});
+let Task = mongoose.model('Task')
+// let task = new Task()
+// task.project = 'Bikeshed'
+// task.description = 'Paint the bikeshed red.'
+// task.save(function (err) {
+//     if (err) throw err
+//     console.log('Task saved.')
+// })
 
-var Task = mongoose.model('Task');
-Task.find({'project': 'Bikeshed'}, function(err, tasks) {
-  for (var i = 0; i < tasks.length; i++) {
-    console.log('ID:' + tasks[i]._id);
-    console.log(tasks[i].description);
-  }
-});
+Task.find({'project': 'Bikeshed'}, function (err, tasks) {
+    console.log(123)
+    for (let i = 0; i < tasks.length; i++) {
+        console.log('ID:' + tasks[i]._id)
+        console.log(tasks[i].description)
+    }
+})
