1d8136d002cc405e7296c12a1006dfa2997ccbbe
jsculsp <328232234@qq.com>
2017-10-12 13:24:02 +0800

    save

--- 6/multi_error.js
+++ 6/multi_error.js
@@ -1,61 +1,61 @@
 var connect = require('connect');
 
 function hello(req, res, next) {
-  if (req.url.match(/^\/hello/)) {
-    res.end('Hello World\n');
-  } else {
-    next();
-  }
+    if (req.url.match(/^\/hello/)) {
+        res.end('Hello World\n');
+    } else {
+        next();
+    }
 }
 
 var db = {
-  users: [
-    { name: 'tobi' },
-    { name: 'loki' },
-    { name: 'jane' }
-  ]
+    users: [
+        {name: 'tobi'},
+        {name: 'loki'},
+        {name: 'jane'}
+    ]
 };
 
 function users(req, res, next) {
-  var match = req.url.match(/^\/user\/(.+)/);
-  if (match) {
-    var user = db.users[match[1]];
-    if (user) {
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(user));
+    var match = req.url.match(/^\/user\/(.+)/);
+    if (match) {
+        var user = db.users[match[1]];
+        if (user) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(user));
+        } else {
+            var err = new Error('User not found');
+            err.notFound = true;
+            next(err);
+        }
     } else {
-      var err = new Error('User not found');
-      err.notFound = true;
-      next(err);
+        next();
     }
-  } else {
-    next();
-  }
 }
 
 function pets(req, res, next) {
-  if (req.url.match(/^\/pet\/(.+)/)) {
-    foo();
-  } else {
-    next();
-  }
+    if (req.url.match(/^\/pet\/(.+)/)) {
+        foo();
+    } else {
+        next();
+    }
 }
 
 function errorHandler(err, req, res, next) {
-  console.error(err.stack);
-  res.setHeader('Content-Type', 'application/json');
-  if (err.notFound) {
-    res.statusCode = 404;
-    res.end(JSON.stringify({ error: err.message }));
-  } else {
-    res.statusCode = 500;
-    res.end(JSON.stringify({ error: 'Internal Server Error' }));
-  }
+    console.error(err.stack);
+    res.setHeader('Content-Type', 'application/json');
+    if (err.notFound) {
+        res.statusCode = 404;
+        res.end(JSON.stringify({error: err.message}));
+    } else {
+        res.statusCode = 500;
+        res.end(JSON.stringify({error: 'Internal Server Error'}));
+    }
 }
 
 connect()
-  .use(users)
-  .use(pets)
-  .use(errorHandler)
-  .use(hello)
-  .listen(3000);
+    .use(users)
+    .use(pets)
+    .use(errorHandler)
+    .use(hello)
+    .listen(3000);
