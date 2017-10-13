802f6a57e5cb86b6cfd573656b634d395bbd37f9
jsculsp <328232234@qq.com>
2017-10-13 09:28:17 +0800

    save

--- 7/method_override/broken.js
+++ 7/method_override/broken.js
@@ -1,24 +1,27 @@
-var connect = require('connect');
+const connect = require('connect')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 function edit(req, res, next) {
-  if ('GET' != req.method) return next();
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<form method="put">');
-  res.write('<input type="text" name="user[name]" value="Tobi" />');
-  res.write('<input type="submit" value="Update" />');
-  res.write('</form>');
-  res.end();
+    if (req.method !== 'GET') return next()
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<form method="put">')
+    res.write('<input type="text" name="user[name]" value="Tobi" />')
+    res.write('<input type="submit" value="Update" />')
+    res.write('</form>')
+    res.end()
 }
 
 function update(req, res, next) {
-  if ('PUT' != req.method) return next();
-  res.end('Updated name to ' + req.body.user.name);
+    if (req.method !== 'PUT') return next()
+    res.end('Updated name to ' + req.body.user.name)
 }
 
-var app = connect()
-  .use(connect.logger('dev'))
-  .use(connect.bodyParser())
-  .use(edit)
-  .use(update);
+const app = connect()
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+app.use(edit)
+app.use(update)
 
-app.listen(3000);
+app.listen(3000)
