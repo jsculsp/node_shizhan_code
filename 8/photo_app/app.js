857d17a9e7b3a2565a990195b10d5cfe60b0d4d4
mu lin <mu lin>
2017-10-15 11:28:18 +0800

    save

--- 8/photo_app/app.js
+++ 8/photo_app/app.js
@@ -5,6 +5,7 @@ const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
+const env = process.env.NODE_ENV = 'development'
 const index = require('./routes/index')
 const users = require('./routes/users')
 
