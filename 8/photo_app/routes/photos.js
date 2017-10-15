43e17f5c75c9cdbca018d91c98ad7f2a4b61ddb1
mu lin <mu lin>
2017-10-15 20:33:48 +0800

    save

--- 8/photo_app/routes/photos.js
+++ 8/photo_app/routes/photos.js
@@ -1,22 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const Photo = require('../models/Photos')
 
-const photos = []
-
-photos.push({
-    name: 'Node.js Logo',
-    path: 'http://nodejs.org/images/logos/nodejs-green.png',
-})
-
-photos.push({
-    name: 'Ryan Speaking',
-    path: 'http://nodejs.org/images/ryan-speaker.jpg',
-})
-
-const list = (req, res) => {
-    res.render('photos', {
-        title: 'Photos',
-        photos: photos,
+const list = (req, res, next) => {
+    Photo.find({}, (err, photos) => {
+        if (err) return next(err)
+        res.render('photos', {
+            title: 'Photos',
+            photos: photos,
+        })
     })
 }
 
