e624eae42094c6210512400624b603aab59f192f
jsculsp <328232234@qq.com>
2017-10-16 11:17:58 +0800

    save

--- shoutbox/app.js
+++ shoutbox/app.js
@@ -1,41 +1,12 @@
 const express = require('express')
-const path = require('path')
-const favicon = require('serve-favicon')
-const logger = require('morgan')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const RedisStore = require('connect-redis')(session)
-
-const index = require('./routes/index')
-const register = require('./routes/register')
 
 // view engine setup
 const app = express()
 module.exports = app
 require('./settings/common')(app)
 
-const hour = 3600000
-
-app.use(session({
-    store: new RedisStore({prefix: 'sid'}),
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: hour * 24,
-    }
-}))
-
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.use('/', index)
-app.use('/register', register)
+app.use('/', require('./routes/index'))
+app.use('/register', require('./routes/register'))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- shoutbox/settings/common.js
+++ shoutbox/settings/common.js
@@ -1,7 +1,33 @@
+const express = require('express')
 const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
 
 module.exports = function (app) {
-    app.set('views', path.join(__dirname, '..', 'views'))
-    app.set('view engine', 'ejs')
-    app.set('root', path.join(__dirname, '..'))
+  app.set('views', path.join(__dirname, '..', 'views'))
+  app.set('view engine', 'ejs')
+  app.set('root', path.join(__dirname, '..'))
+
+  const hour = 3600000
+
+  app.use(session({
+    store: new RedisStore({prefix: 'sid'}),
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: hour * 24,
+    }
+  }))
+
+  app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')))
+  app.use(logger('dev'))
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({extended: false}))
+  app.use(cookieParser())
+  app.use(express.static(path.join(__dirname, '..', 'public')))
 }
