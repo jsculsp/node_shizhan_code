58ff2b47c6d7028226ec15774d1395b1ecf18415
jsculsp <328232234@qq.com>
2017-10-16 13:18:44 +0800

    save

--- shoutbox/routes/index.js
+++ shoutbox/routes/index.js
@@ -1,9 +1,40 @@
 const express = require('express')
+const User = require('../lib/user')
 const router = express.Router()
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
+const index = (req, res, next) => {
   res.render('index', {title: 'Express'})
-})
+}
+
+const form = (req, res, next) => {
+  res.render('login', {title: 'Login'})
+}
+
+const submit = (req, res, next) => {
+  let username = req.body.username
+  let password = req.body.password
+  User.authenticate(username, password, (err, user) => {
+    if (err) next(err)
+    if (user) {
+      req.session.uid = user.id
+      res.redirect('/')
+    } else {
+      res.error('Sorry! invalid credentials.')
+      res.redirect('back')
+    }
+  })
+}
+
+const logout = (req, res, next) => {
+  req.session.destory((err) => {
+    if (err) throw err
+    res.redirect('/')
+  })
+}
+
+router.get('/', index)
+router.get('/login', form)
+router.post('/login', submit)
+router.get('/logout', logout)
 
 module.exports = router
--- shoutbox/settings/common.js
+++ shoutbox/settings/common.js
@@ -31,5 +31,5 @@ module.exports = function (app) {
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({extended: false}))
   app.use(cookieParser())
-  app.use('/public', express.static(path.join(__dirname, '..', 'public')))
+  app.use(express.static(path.join(__dirname, '..', 'public')))
 }
