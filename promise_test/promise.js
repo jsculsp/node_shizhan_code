6e92b11961939939948f6c91645b68777a4fa3c5
jsculsp <328232234@qq.com>
2017-10-25 11:07:04 +0800

    save

---
+++ promise_test/promise.js
@@ -0,0 +1,45 @@
+const events = require('events')
+const EventEmitter = events.EventEmitter
+const log = console.log.bind(console)
+
+class Promise extends EventEmitter {
+  constructor() {
+    super()
+  }
+
+  then(fullfilledHandler, errorHandler, progressHandler) {
+    if (typeof fullfilledHandler === 'function') {
+      this.once('success', fullfilledHandler)
+    }
+    if (typeof errorHandler === 'function') {
+      this.once('error', errorHandler)
+    }
+    if (typeof progressHandler === 'function') {
+      this.on('progress', progressHandler)
+    }
+    return this
+  }
+}
+
+class Deffered {
+  constructor() {
+    this.state = 'unfulfilled'
+    this.promise = new Promise()
+  }
+
+  resolve(obj) {
+    this.state = 'fulfilled'
+    this.promise.emit('success', obj)
+  }
+
+  reject(obj) {
+    this.state = 'failed'
+    this.promise.emit('error', err)
+  }
+
+  progress(data) {
+    this.promise.emit('progress', data)
+  }
+}
+
+exports.Deffered = Deffered
