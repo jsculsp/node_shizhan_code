a23c36afdba9526230baab5aaec802c652b944c1
jsculsp <328232234@qq.com>
2017-10-26 13:27:04 +0800

    save

---
+++ promise_test/basic_test.js
@@ -0,0 +1,19 @@
+var assert = require('assert')
+describe('Basic Test', function () {
+    context('When Callback(high-order function)', function () {
+        it('should use `done` for test', function (done) {
+            setTimeout(function () {
+                assert(true)
+                done()
+            }, 0)
+        })
+    })
+    context('When promise object', function () {
+        it('should use `done` for test?', function (done) {
+            var promise = Promise.resolve(1)
+            promise.then(function (value) {
+                assert(value === 2)
+            }).then(done, done)
+        })
+    })
+})
