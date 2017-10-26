fea6b052b51898b038ee7de6d67a121c0ba2ae0e
jsculsp <328232234@qq.com>
2017-10-26 16:05:59 +0800

    save

--- promise_test/simple_timeout_promise.js
+++ promise_test/simple_timeout_promise.js
@@ -1,3 +1,5 @@
+class TimeoutError extends Error {}
+
 function delayPromise(ms) {
   return new Promise(function (resolve) {
     setTimeout(resolve, ms)
@@ -6,7 +8,7 @@ function delayPromise(ms) {
 
 function timeoutPromise(promise, ms) {
   var timeout = delayPromise(ms).then(function () {
-    return Promise.reject(new Error('Operation timed out after ' + ms + ' ms'))
+    return Promise.reject(new TimeoutError('timeout'))
   })
   return Promise.race([promise, timeout])
 }
