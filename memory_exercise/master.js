5d2c7fb245cd25fa8d36715ddd21381d7f740b6b
jsculsp <328232234@qq.com>
2017-10-31 14:21:14 +0800

    save

--- memory_exercise/master.js
+++ memory_exercise/master.js
@@ -1,11 +1,28 @@
 const fork = require('child_process').fork
 const cpus = require('os').cpus()
-
 const server = require('net').createServer()
 server.listen(3000)
 
 const workers = {}
+const limit = 10
+const during = 60000
+let restart = []
+
+const isTooFrequently = function () {
+  let time = Date.now()
+  let length = restart.push(time)
+  if (length > limit) {
+    restart = restart.slice(-limit)
+  }
+  return restart.length >= limit && restart[restart.length - 1] - restart[0] < during
+}
+
 const createWorker = function () {
+  // 检查是否太过频繁
+  if (isTooFrequently()) {
+    process.emit('giveup', length, during)
+    return
+  }
   let worker = fork(`${__dirname}/worker.js`)
   worker.on('message', function (message) {
     if (message.act === 'suicide') {
@@ -29,4 +46,8 @@ process.on('exit', function () {
   for (let pid in workers) {
     workers[pid].kill()
   }
+})
+
+process.on('giveup', function (length, during) {
+  // 在这个事件中添加重要日志，并让监控系统监视到这个严重错误，进而报警等。
 })
