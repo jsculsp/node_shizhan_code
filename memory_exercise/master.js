2c5fdf7c9258cc5b600a72a7f3bf5b5a574f544e
jsculsp <328232234@qq.com>
2017-10-31 14:06:39 +0800

    save

--- memory_exercise/master.js
+++ memory_exercise/master.js
@@ -1,6 +1,32 @@
 const fork = require('child_process').fork
 const cpus = require('os').cpus()
 
-for (let i = 0; i < cpus.length; i++) {
-  fork('./worker.js')
+const server = require('net').createServer()
+server.listen(3000)
+
+const workers = {}
+const createWorker = function () {
+  let worker = fork(`${__dirname}/worker.js`)
+  worker.on('message', function (message) {
+    if (message.act === 'suicide') {
+      createWorker()
+    }
+  })
+  worker.on('exit', function () {
+    console.log(`Worker ${worker.pid} exited.`)
+    delete workers[worker.pid]
+  })
+  worker.send('server', server)
+  workers[worker.pid] = worker
+  console.log(`Create worker. pid: ${worker.pid}`)
 }
+
+for (let i = 0; i < cpus.length; i++) {
+  createWorker()
+}
+
+process.on('exit', function () {
+  for (let pid in workers) {
+    workers[pid].kill()
+  }
+})
--- memory_exercise/worker.js
+++ memory_exercise/worker.js
@@ -1,10 +1,26 @@
 const http = require('http')
 
 const server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello World\n')
+  res.writeHead(200, {'Content-type': 'text/plain'})
+  res.end(`handled by child, pid is ${process.pid}\n`)
+  throw new Error('throw exception')
 })
 
-let port = Math.round(1000 + Math.random() * 1000)
-server.listen(port, '0.0.0.0')
-console.log('listening at: ' + port)
+let worker
+process.on('message', function (m, tcp) {
+  worker = tcp
+  if (m === 'server') {
+    worker.on('connection', function (socket) {
+      server.emit('connection', socket)
+    })
+  }
+})
+
+process.on('uncaughtException', function () {
+  process.send({act: 'suicide'})
+  // 停止接收新的连接
+  worker.close(function () {
+    // 所有已有连接断开后，退出进程
+    process.exit(1)
+  })
+})
